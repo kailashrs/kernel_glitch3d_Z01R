0b737cecf08bd5fb747a04483d15e78d0832d9cc
Kailash <kailash.sudhakar@gmail.com>
2020-03-15 02:38:30 +0530

    Switch to newer GCC

--- build.sh
+++ build.sh
@@ -1,8 +1,8 @@
 make clean
 export ARCH="arm64"
 export PATH="/usr/lib/ccache/bin:$PATH"
-export CROSS_COMPILE="ccache /home/kailashrs/5z/aarch64-elf-gcc/bin/aarch64-elf-"
-export CROSS_COMPILE_ARM32="ccache /home/kailashrs/5z/arm-eabi-gcc/bin/arm-eabi-"
+export CROSS_COMPILE="ccache /home/kailashrs/5z/arm64-gcc/bin/aarch64-elf-"
+export CROSS_COMPILE_ARM32="ccache /home/kailashrs/5z/arm32-gcc/bin/arm-eabi-"
 make O=out Z01R_defconfig
 make O=out -j$(nproc --all)
 cd ..
